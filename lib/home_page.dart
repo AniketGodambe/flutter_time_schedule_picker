10ea8ceb3324ce8c74de3336cbd5bbf7164c43c6
Aniket Godambe <godambeaniket07@gmail.com>
2022-12-28 12:00:05 +0530

    done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -51,10 +51,123 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: const [],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Center(
+                  child: CommonText(
+                    text: 'Select time',
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(
+                            color: Colors.black,
+                          )),
+                      child: GestureDetector(
+                        onTap: () => selectStartTime(),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              startTime.format(context).toString(),
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                            const Icon(
+                              Icons.access_time_outlined,
+                              color: Colors.black,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 40),
+                    const CommonText(
+                      text: 'To',
+                      color: Colors.grey,
+                      fontSize: 18,
+                    ),
+                    const SizedBox(width: 40),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(
+                            color: Colors.black,
+                          )),
+                      child: GestureDetector(
+                        onTap: () => selectEndTime(),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              endTime.format(context).toString(),
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                            const Icon(
+                              Icons.access_time_outlined,
+                              color: Colors.black,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                CommonText(
+                    text:
+                        "Your session will start at ${startTime.format(context).toString()} ",
+                    fontSize: 18,
+                    color: Colors.black),
+                const SizedBox(height: 5),
+                CommonText(
+                    text:
+                        "and will end at ${endTime.format(context).toString()}",
+                    fontSize: 18,
+                    color: Colors.black),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class CommonText extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  final Color color;
+  const CommonText(
+      {super.key,
+      required this.text,
+      required this.fontSize,
+      required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize,
+        fontWeight: FontWeight.bold,
+        color: color,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,11 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
